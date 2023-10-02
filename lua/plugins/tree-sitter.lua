a381c8d6adca8a8c8c7f64b51e0780444ac1f9db
AndresDev711 <andres7condori@gmail.com>
2023-10-02 15:23:20 -0500

    add treesitter and lsp config

---
+++ lua/plugins/tree-sitter.lua
@@ -0,0 +1,37 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	build = ":TSUpdate",
+	main = "nvim-treesitter.configs",
+	opts = {
+		ensure_installed = {
+			"lua",
+			"luadoc",
+		},
+		highlight = {
+			enable = true,
+			disable = { "c", "cpp", "bash"},
+		},
+		indent = {
+			enable = true,
+		},
+		textobjexts = {
+			select = {
+				enable = true,
+				lookahead = true,
+				keymaps = {
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@conditional.outer",
+					["ic"] = "@conditional.inner",
+					["al"] = "@loop.outer",
+					["il"] = "@loop.inner",
+				}
+			}
+		}
+			
+	},
+
+}
